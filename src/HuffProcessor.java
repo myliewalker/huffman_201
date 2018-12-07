59d94a6998f023dd81b88ba1963d1c3bad894614
Mylie Walker <myliewalker@Mylies-MacBook-Pro.local>
2018-12-06 21:48:58 -0500

    removed print statements

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -64,7 +64,7 @@ public class HuffProcessor {
 		int[] freq = new int[ALPH_SIZE + 1];
 		while (true){
 			int val = in.readBits(BITS_PER_WORD);
-			System.out.print(val + " ");
+			// System.out.print(val + " ");
 			if (val == -1) break;
 			freq[val]++;
 		}
@@ -79,9 +79,9 @@ public class HuffProcessor {
 				pq.add(new HuffNode(i, counts[i], null, null));
 			}
 		}
-		if (myDebugLevel >= DEBUG_HIGH) {
-			System.out.printf("pq created with %d nodes \n", pq.size());
-		}
+		// if (myDebugLevel >= DEBUG_HIGH) {
+		// 	System.out.printf("pq created with %d nodes \n", pq.size());
+		// }
 		while (pq.size() > 1) {
 			HuffNode left = pq.remove();
 			HuffNode right = pq.remove();
@@ -102,9 +102,9 @@ public class HuffProcessor {
 		if (root == null) return;
 		if (root.myLeft == null && root.myRight == null) {
 			encodings[root.myValue] = path;
-			if (myDebugLevel >= DEBUG_HIGH) {
-				System.out.printf("encoding for %d is %s\n", root.myValue, path);
-			}
+			// if (myDebugLevel >= DEBUG_HIGH) {
+			// 	System.out.printf("encoding for %d is %s\n", root.myValue, path);
+			// }
 			return;	
 		}
 		codingHelper(root.myLeft, path+"0", encodings);
